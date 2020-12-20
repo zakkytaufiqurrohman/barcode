3ea823a7102c023d9501723d3e6f316e6bbb19ca
zakky <taufiqurrohmmanzq@gmail.com>
2020-12-20 14:12:14 +0700

    soft delete

--- app/Models/Berkas.php
+++ app/Models/Berkas.php
@@ -3,12 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class Berkas extends Model
 {
+    use SoftDeletes;
+
     protected $table = 'tbl_berkas';
     protected $primaryKey = 'id_berkas';
     protected $fillable = ['tipe_berkas','id_user','tanggal','waktu','password_berkas', 'password', 'kode_berkas', 'created_at','updated_at'];
+    protected $dates = ['deleted_at'];
 
     public function users()
     {

b5fd8b6c36bdc20adeb8c2795e6c37558cb94176
Mufid Ardianto <ardianto219@gmail.com>
2020-12-20 16:34:31 +0700

    merapikan tampilan login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -30,11 +30,11 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="{{ url('') }}/index2.html"><b>Admin</b>LTE</a>
+    <img src="{{ url('') }}/logo-ini.png" class="brand-image" style="float: none;max-height: 120px">
   </div>
   <!-- /.login-logo -->
   <div class="login-box-body">
-    <p class="login-box-msg">Sign in to start your session</p>
+    <p class="login-box-msg">Login</p>
 
     <form action="javascript:void(0);" class="needs-validation" novalidate="" method="post" id="form-login">
         @csrf
@@ -46,17 +46,10 @@
         <input id="password_user" type="password" class="form-control" name="password" placeholder="Password">
         <span class="glyphicon glyphicon-lock form-control-feedback"></span>
       </div>
-      <div class="row">
-        <div class="col-xs-8">
-          <div class="checkbox icheck">
-            <label>
-              <input type="checkbox"> Remember Me
-            </label>
-          </div>
-        </div>
+      <div class="form-group has-feedback">
         <!-- /.col -->
-        <div class="col-xs-4">
-          <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+        <div class="col-12">
+          <button type="submit" class="btn btn-primary btn-block btn-flat">Masuk</button>
         </div>
         <!-- /.col -->
       </div>
